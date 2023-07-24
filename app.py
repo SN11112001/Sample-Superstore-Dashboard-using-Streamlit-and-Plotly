3aa872da019b73d20f4c0ec4d03c017b757145e6
Saurabh Narwane <58877807+SN11112001@users.noreply.github.com>
2023-07-24 14:10:43 +0530

    app.py

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ with h2.container():
                 default_index = 1,
                 orientation = "horizontal",
                 styles={
-                "container": {"text-align": "left"},
+                "container": {"font-family": "sans serif", "text-align": "left"},
                 "nav-link": {"font-size": "15px", "text-align": "center"}
                 }
             )
